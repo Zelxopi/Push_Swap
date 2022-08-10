47d95d73e8a66b628432c0da9277904632c6d663
Maya Tremblay <zelxopi@lilbee.lan>
2022-08-10 13:15:33 -0400

    now handling all type of args

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mtrembla <mtrembla@student.42quebec>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/19 10:34:29 by mtrembla          #+#    #+#             */
-/*   Updated: 2022/08/10 12:27:00 by mtrembla         ###   ########.fr       */
+/*   Updated: 2022/08/10 13:14:11 by mtrembla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,25 @@
 
 void create_list(int argc, char **argv, t_stack *stack_a)
 {
-	int			i;
-	int			value;
+	int		i;
+	int		j;
+	int		value;
+	char	**args;
 
 	check_twin(argv);
-	i = 0;
-	while (argv[i++] && i < argc)
+	i = 1;
+	j = 0;
+	while (argv[i] && i < argc)
 	{
-		ft_is_char_digit(argv[i]);
-		value = ft_atoi(argv[i]);
-		dlist_add_back(stack_a, value);
+		args = ft_split(argv[i++], ' ');
+		check_twin(args);
+		while (args[j])
+		{
+			ft_is_char_digit(args[j]);
+			value = ft_atoi(args[j++]);
+			dlist_add_back(stack_a, value);
+		}
+		j = 0;
 	}
 }
 
@@ -36,10 +45,8 @@ int	main(int argc, char **argv)
 	
 	if (argc < 2)
 		ft_error("Not enough arguments");
-	if (argc > 2)
+	if (argc >= 2)
 		create_list(argc, argv, &stack_a);
-	if (argc == 2)
-		return(0);
 	view(stack_a);
 	return(0);
 }
